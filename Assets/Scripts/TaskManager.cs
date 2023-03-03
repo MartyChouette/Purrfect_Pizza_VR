6332311936a13b488595f8d69954d20e6ff13361
bmoya13 <71987706+bmoya13@users.noreply.github.com>
2023-03-03 14:35:22 -0800

    Task Display and Task Managers

---
+++ Assets/Scripts/TaskManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskManager : MonoBehaviour
+{
+
+    public string[] tasks = new string[] {
+        "Cut Onions",
+        "Grill Meat",
+        "Cook Eggs",
+        "Mash Potatoes",
+        "Sear Veggies"
+    };
+
+    public ChefTaskY chefY;
+    public ChefTaskZ chefZ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
