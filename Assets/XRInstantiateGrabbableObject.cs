5db19e3bdd10ee3333cbce4f264dc091c3e69363
bmoya13 <71987706+bmoya13@users.noreply.github.com>
2023-03-29 19:14:51 -0700

    conveyor script, toppings bin
    still need to test instantiate of topping bin, pushing for now

---
+++ Assets/XRInstantiateGrabbableObject.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine;
+
+public class XRInstantiateGrabbableObject : XRBaseInteractable
+{
+
+    public GameObject grabbableObject;
+
+    public Transform transformToInstantiate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        //Instantiate object
+        GameObject newObject = Instantiate(grabbableObject, transformToInstantiate.position, Quaternion.identity);
+
+        //Get grab interactable from prefab
+        XRGrabInteractable objectInteractable = newObject.GetComponent<XRGrabInteractable>();
+
+        //Select object into same interactor
+        interactionManager.SelectEnter(args.interactorObject, objectInteractable);
+
+        base.OnSelectEntered(args);
+    }
+
+
+}
