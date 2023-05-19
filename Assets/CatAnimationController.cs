654d1b018ee06b75ab004a9eee5b7e643a3da32a
dennisongaia <77359793+dennisongaia@users.noreply.github.com>
2023-05-19 12:00:58 -0700

    cat animation

---
+++ Assets/CatAnimationController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAnimationController : MonoBehaviour
+{
+    public Animator animator;
+    public float animationDuration = 20f;
+    private float timer = 0f;
+    private bool isAnimating = false;
+    
+
+    private void Update()
+    {
+        if (!isAnimating)
+        {
+            // Start the animation
+            isAnimating = true;
+            animator.SetBool("isCooking", true);
+            timer = 0f;
+        }
+
+        if (isAnimating)
+        {
+            timer += Time.deltaTime;
+            if (timer >= animationDuration)
+            {
+                // Stop the animation
+                animator.SetBool("isCooking", false);
+                animator.SetBool("doneCooking", true);
+                isAnimating = false;
+            }
+        }
+    }
+}
